65b46d196712c4089e0f5c90399d3a76bbc83080
MAN-MARU <150600902+MAN-MARU@users.noreply.github.com>
2024-03-27 08:26:26 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,17 @@
+// 変数の宣言
+let userWeight;
+
+// 体重の代入
+userWeight = 68;
+
+// 変数の宣言
+let userHight;
+
+// 身長の代入
+userHight = 1.7;
+
+//bmi
+let bmi = userWeight/(userHight*userHight);
+
+// BMIの計算結果をコンソールへ出力
+console.log(bmi);
